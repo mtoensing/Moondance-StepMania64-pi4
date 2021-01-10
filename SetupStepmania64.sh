1eed43967c9c13edb680472ca7026fef5e8694f1
Marc Toensing <marc.toensing@gmail.com>
2021-01-10 16:33:16 +0100

    better script

--- SetupStepmania64.sh
+++ SetupStepmania64.sh
@@ -4,6 +4,8 @@
 echo "Moondance StepMania64 installation script V1.0"
 echo "Latest version always at https://github.com/mtoensing/Moondance-StepMania64-pi4"
 
+cd
+
 if [ -d "Stepmania64" ]; then
   echo "Directory Stepmania64 already exists!  Exiting... "
   exit 1
@@ -13,5 +15,11 @@ echo "Updating packages..."
 sudo apt-get update
 sudo apt-get upgrade
 
+echo "Installing git..."
+sudo apt-get install git
+
 echo "Installing latest libopengl0..."
 sudo apt install libopengl0
+
+echo "Cloning repository"
+git clone https://github.com/mtoensing/Moondance-StepMania64-pi4.git
